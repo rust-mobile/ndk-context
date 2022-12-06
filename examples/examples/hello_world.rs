4eba6ca333b6efe486bcbd2c291efd69739b5e6a
Robert Bragg <robert@sixbynine.org>
2022-12-06 18:29:57 +0000

    Update examples for standalone repo
    - Drops the looper.rs example which isn't relevant for demonstrating
      the `ndk_context` crate
    - Ports examples from ndk-glue to android-activity
    - Adds some minimal usage of `ndk_context` + `jni-rs` to the
      hello_world.rs example which will be run by CI

--- examples/examples/hello_world.rs
+++ examples/examples/hello_world.rs
@@ -1,14 +1,31 @@
+use android_activity::AndroidApp;
+use jni::objects::{JObject, JValue};
 use log::info;
-use ndk::trace;
 
-#[cfg_attr(
-    target_os = "android",
-    ndk_glue::main(backtrace = "on", logger(level = "debug", tag = "hello-world"))
-)]
-fn main() {
-    let _trace;
-    if trace::is_trace_enabled() {
-        _trace = trace::Section::new("ndk-rs example main").unwrap();
-    }
+/// A minimal example of how to use `ndk_context` to get a `JavaVM` + `Context and make a JNI call
+fn ndk_context_jni_test() -> Result<(), Box<dyn std::error::Error>> {
+    // Get a VM for executing JNI calls
+    let ctx = ndk_context::android_context();
+    let vm = unsafe { jni::JavaVM::from_raw(ctx.vm().cast()) }?;
+    let _context = unsafe { JObject::from_raw(ctx.context().cast()) };
+    let env = vm.attach_current_thread()?;
+
+    // Since we aren't making JNI calls within the implementation of a native call from the JavaVM
+    // we wrap the reference in an `AutoLocal` to make sure it will be deleted.
+    let _int_ref = env.auto_local(
+        env.new_object("java/lang/Integer", "(I)V", &[JValue::Int(42)])
+            .unwrap(),
+    );
+
+    Ok(())
+}
+
+#[no_mangle]
+fn android_main(_app: AndroidApp) {
+    android_logger::init_once(android_logger::Config::default().with_min_level(log::Level::Info));
+
+    ndk_context_jni_test().unwrap();
+
     info!("hello world");
+    println!("hello world");
 }
